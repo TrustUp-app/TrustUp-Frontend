a03eaea0c45d7d9e1da1dd792df14bcb20e06dee
D240021 <danielbarrantesq@gmail.com>
2026-01-12 12:32:48 -0600

    Init project added

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import PayScreen from 'components/pages/PayScreen';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
+      {/* <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+      <StatusBar style="auto" /> */}
+      <PayScreen/>
     </>
   );
 }
---
+++ components/pages/PayScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function PayScreen() {
+  return (
+   
+    <View className='w-100 flex-1 justify-center items-center'>
+        <Text className='text-red-700'>Pay Screen</Text>
+    </View>
+
+  )
+}
