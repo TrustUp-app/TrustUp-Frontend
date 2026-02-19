a0e94af31f72f14577cb96c45c2b39f425d5bcac
Emmzyemms <friyoemmy@gmail.com>
2026-02-19 17:59:58 +0100

    Feat: Create Invest in TrustUp page UI

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import CreateAccountScreen from 'components/pages/CreateAccountScreen';
+import InvestScreen from 'components/pages/InvestScreen';
 import './global.css';
 
 export default function App() {
-   return <CreateAccountScreen />;
+   return <InvestScreen />;
 }
