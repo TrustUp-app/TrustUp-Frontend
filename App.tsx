0862082c9d1e1977cdf8b6f6cc8fc83dace0b893
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2026-03-03 09:35:06 +0100

    feat: merge conflict fix

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import InvestScreen from 'components/pages/InvestScreen';
 import SettingsScreen from 'components/pages/SettingsScreen';
 import { RootStackParamList } from 'types/Navigation';
@@ -9,11 +10,13 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Invest Screen" component={InvestScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Invest Screen" component={InvestScreen} />
+          <Stack.Screen name="Settings" component={SettingsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
